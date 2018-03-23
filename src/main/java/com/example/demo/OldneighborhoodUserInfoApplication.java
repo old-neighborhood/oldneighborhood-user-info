19d7447fb2bd2f27eded5e544938bc70f4dc251e
lzy141250084 <141250084@smail.nju.edu.cn>
2018-03-23 09:59:21 +0800

    创建项目

---
+++ src/main/java/com/example/demo/OldneighborhoodUserInfoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OldneighborhoodUserInfoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OldneighborhoodUserInfoApplication.class, args);
+	}
+}
